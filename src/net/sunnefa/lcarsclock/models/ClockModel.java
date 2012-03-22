85dbf15ef0c255afebda74ee37b7697d2dfa0126
Sunnefa Lind <sunnefa_lind@hotmail.com>
2012-03-22 18:44:19 +0000

    Created the clock model Added the code to the clock model to start a date object and return the time variables. Created the AbstractClock class which the views will extend.

--- src/net/sunnefa/lcarsclock/models/ClockModel.java
+++ src/net/sunnefa/lcarsclock/models/ClockModel.java
@@ -5,10 +5,53 @@
  */
 package net.sunnefa.lcarsclock.models;
 
+import java.util.Date;
+import java.util.Timer;
+
+
+
 /**
  *
  * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
  */
 public class ClockModel {
+    
+    Date clock_date;
+    
+    public ClockModel() {
+        this.clock_date = new Date(2371, 5, 25, 12, 15);
+    }
+    
+    public int get_seconds() {
+        return this.clock_date.getSeconds();
+    }
+    
+    public int get_minutes() {
+        return this.clock_date.getMinutes();
+    }
+    
+    public int get_hours() {
+        return this.clock_date.getHours();
+    }
+    
+    public int get_date() {
+        console(this.clock_date.getDate());
+        return this.clock_date.getDate();
+    }
+    
+    public int get_year() {
+        console(this.clock_date.getYear());
+        return this.clock_date.getYear();
+    }
+    
+    public int get_month() {
+        console(this.clock_date.getMonth());
+        return this.clock_date.getMonth();
+    }
+    
+    private void console(int num) {
+        System.out.println(num);
+    }
+
     
 }
--- src/net/sunnefa/lcarsclock/views/DigitalView.java
+++ src/net/sunnefa/lcarsclock/views/DigitalView.java
@@ -5,50 +5,52 @@
  */
 package net.sunnefa.lcarsclock.views;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import net.sunnefa.lcarsclock.models.ClockModel;
 
 /**
  *
  * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
  */
-public class DigitalView extends JPanel {
+public class DigitalView extends AbstractClock {
     
-    ClockModel clock_model;
+    JLabel time_label = new JLabel();
+    
+    JLabel date_label = new JLabel();
+    
+    JLabel star_label = new JLabel("", JLabel.CENTER);
     
     public DigitalView(ClockModel clock_model) {
-        this.clock_model = clock_model;
+        super(clock_model);
+        
+        GridLayout grid = new GridLayout(2,1);
+        grid.setHgap(150);
+        
+        this.setLayout(grid);
+        
+        this.time_label = new JLabel();
+        this.time_label.setText(this.clock_model.get_hours() + ":" + this.clock_model.get_minutes() + ":" + this.clock_model.get_seconds());
+        this.time_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
+        this.time_label.setForeground(new Color(255, 153, 0));
+        this.time_label.setBounds(100, 100, 300, 300);
+        
+        this.date_label = new JLabel();
+        this.date_label.setText(this.clock_model.get_date() + "." + this.clock_model.get_month() + "." + this.clock_model.get_year());
+        this.date_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
+        this.date_label.setForeground(new Color(255, 153, 0));
+        
+        this.star_label = new JLabel("", JLabel.CENTER);
+        this.star_label.setText("65582.5");
+        this.star_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
+        this.star_label.setForeground(new Color(255, 153, 0));
         
         
-        
-        //GridLayout grid = new GridLayout(2,1);
-        
-        this.setLayout(new BorderLayout());
-        
-        JLabel time_label = new JLabel();
-        time_label.setText("15:47:10");
-        time_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
-        time_label.setForeground(new Color(255, 153, 0));
-        time_label.setBounds(100, 100, 300, 300);
-        
-        JLabel date_label = new JLabel();
-        date_label.setText("22.03.2012");
-        date_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
-        date_label.setForeground(new Color(255, 153, 0));
-        
-        JLabel star_label = new JLabel();
-        star_label.setText("65582.5");
-        star_label.setFont(new Font("Swiss911 UCm BT", Font.TRUETYPE_FONT, 79));
-        star_label.setForeground(new Color(255, 153, 0));
-        
-        
-        this.add(time_label, BorderLayout.WEST);
-        this.add(date_label, BorderLayout.EAST);
-        this.add(star_label, BorderLayout.SOUTH);
+        this.add(this.time_label);
+        this.add(this.date_label);
+        this.add(this.star_label);
         
     }
     
