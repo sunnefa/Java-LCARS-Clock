e66da9d3aae1bfbc55aca6bf7ec520b534a9e9bb
Sunnefa Lind <sunnefa_lind@hotmail.com>
2012-03-22 14:06:21 +0000

    Added some comments Added JavaDoc style comments to the LCARSClock.java class

--- src/net/sunnefa/lcarsclock/LCARSClock.java
+++ src/net/sunnefa/lcarsclock/LCARSClock.java
@@ -6,11 +6,15 @@
 package net.sunnefa.lcarsclock;
 
 /**
- *
+ * This is the main class in my LCARS Clock app
  * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
  */
 public class LCARSClock {
     
+    /**
+     * Standard main method, instantiates the LClock class
+     * @param args 
+     */
     public static void main(String[] args) {
         LClock clock = new LClock();
     }
