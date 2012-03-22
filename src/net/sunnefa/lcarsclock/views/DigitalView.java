ad20f9f43152729a5a63201000115f7866f9b4a1
Sunnefa Lind <sunnefa_lind@hotmail.com>
2012-03-22 15:48:59 +0000

    Set the font and the size for the label in DigitalView

--- src/net/sunnefa/lcarsclock/views/DigitalView.java
+++ src/net/sunnefa/lcarsclock/views/DigitalView.java
@@ -5,6 +5,8 @@
  */
 package net.sunnefa.lcarsclock.views;
 
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import net.sunnefa.lcarsclock.models.ClockModel;
@@ -21,8 +23,9 @@ public class DigitalView extends JPanel {
         this.clock_model = clock_model;
         
         JLabel time_label = new JLabel();
-        time_label.setText("I'm a time label");
-        
+        time_label.setText("15:47:10");
+        time_label.setFont(new Font("/Users/shannensekaya/NetBeansProjects/LCARS Clock/assets/lcars.TTF", Font.BOLD, 48));
+        time_label.setForeground(new Color(255, 0, 0));
         this.add(time_label);
         
     }
